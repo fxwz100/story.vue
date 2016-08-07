0d9e9f4efb58de75d8c1ed6fbf0760ca9937afc2
Zhi <yfwz100@yeah.net>
2016-08-07 15:06:31 +0800

    the demo.

---
+++ game.js
@@ -0,0 +1,57 @@
+Vue.transition('fade', {
+  enterClass: 'fadeIn',
+  leaveClass: 'fadeOut'
+});
+
+var app = new Vue({
+  el: '#story-frame',
+  data: {
+    scripts: [],
+    idx: 0,
+    subIdx: 0
+  },
+  computed: {
+    current: function () {
+      return this.scripts[this.idx];
+    }
+  },
+  methods: {
+    init: function (url) {
+      var app = this;
+      fetch(url).then(function (r) { return r.json(); }).then(function (scripts) {
+        app.scripts = scripts;
+        app.idx = 0;
+        app.subIdx = 0;
+      }); 
+    },
+    next: function () {
+      var nextFrame = this.scripts[this.idx + 1];
+      if (nextFrame.type == 'init') {
+        this.init(nextFrame.url);
+      } else {
+        this.idx += 1;
+      }
+    },
+    nextSubDialog: function () {
+      if (this.subIdx < this.current.content.length - 1) {
+        this.subIdx++;
+      } else {
+        this.next();
+        this.subIdx = 0;
+      }
+    },
+    menu: function (cmd) {
+      if (cmd) {
+        var params = cmd.split(' ');
+        var method = params.shift();
+        this[method].apply(this, params);
+      }
+    },
+    goto: function (idx, subIdx) {
+      this.idx = parseInt(idx);
+      this.subIdx = parseInt(subIdx) || 0;
+    }
+  }
+});
+
+app.init('assets/scene/0/scripts.json')
